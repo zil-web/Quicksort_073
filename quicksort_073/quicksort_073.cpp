85f69ec3af84b55fa4337bf7bb68cf52ba3acdce
zil-web <126854462+zil-web@users.noreply.github.com>
2023-03-31 15:03:48 +0700

    swaps the element at index x with the element at index y

--- quicksort_073/quicksort_073.cpp
+++ quicksort_073/quicksort_073.cpp
@@ -28,3 +28,29 @@ void input() {
 		cin >> arr[i];
 	}
 }
+//swaps the element at index x with the element at index y
+void swap(int x, int y)
+{
+	int temp;
+
+	temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
+}
+
+void q_short(int low, int high)
+{
+	int pivot, i, j;
+	if (low > high)					//langkah 1
+		return;
+
+	//Partition the list into two parts:
+	//One containing elements less that or equal to pivot
+	//Outher conntainning elements greather than pivot 
+
+	pivot = arr[low];				//langkah 2
+
+	i = low + 1;					//langkah 3
+	j = high;						//langkah 4
+
+
