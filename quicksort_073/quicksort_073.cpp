aa43fa14db14fbaa53c3f644aa53bab2f8548226
zil-web <126854462+zil-web@users.noreply.github.com>
2023-03-31 15:04:26 +0700

    Search for an element greater than pivot

--- quicksort_073/quicksort_073.cpp
+++ quicksort_073/quicksort_073.cpp
@@ -52,5 +52,30 @@ void q_short(int low, int high)
 
 	i = low + 1;					//langkah 3
 	j = high;						//langkah 4
+	
+	while (i <= j)					//langkah 10
+	{
+		//Search for an element greater than pivot
+		while ((arr[i] <= pivot) && (i <= high))	//langkah 5
+		{
+			i++;									//langkah 6
+			cmp_count++;
+		}
+		cmp_count++;
+		//search for an element less than or equal to pivot 
+		while ((arr[j] > pivot) && (j >= low))		//langkah 7
+		{
+			j--;									//langkah 8
+			cmp_count++;
+		}
+		cmp_count++;
 
+		//Langkah 9
+		if (i < j) // if the greater element is on the left of the element
+		{
+			//swap the element at index i whit the element at index j
+			swap(i, j);
+			mov_count++;
+		}
+	}
 
