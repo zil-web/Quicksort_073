5fd59e8df5558f904222ecacdcdaeca46f8f5cdf
zil-web <126854462+zil-web@users.noreply.github.com>
2023-03-31 15:05:43 +0700

    Number of data movements

--- quicksort_073/quicksort_073.cpp
+++ quicksort_073/quicksort_073.cpp
@@ -93,6 +93,19 @@ void q_short(int low, int high)
 
 
 }
+void display() {
+	cout << "\n------------------" << endl;
+	cout << "Sorted Array" << endl;
+	cout << "------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << "\n\nNumber of comparasions: " << cmp_count << endl;
+	cout << "Number of data movements: " << mov_count << endl;
+}
 
 
 
