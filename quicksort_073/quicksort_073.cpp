b3b3436ed600f8f71c29de8808482b232b51d97b
zil-web <126854462+zil-web@users.noreply.github.com>
2023-03-31 15:02:26 +0700

    Masukkan panajng element array

--- quicksort_073/quicksort_073.cpp
+++ quicksort_073/quicksort_073.cpp
@@ -6,3 +6,16 @@ int arr[20];
 int cmp_count = 0; // number of comprasion
 int mov_count = 0; // number of data movemnts
 int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukkan panajng element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+
